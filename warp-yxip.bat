4aeabde0779c93d3b817fb3900c5976b7e5b8db7
Milad Biglarian <milad7091@gmail.com>
2024-08-25 14:42:13 +0330

    Add files via upload

---
+++ warp-yxip.bat
@@ -0,0 +1,105 @@
+@echo off
+chcp 936 >nul
+setlocal enabledelayedexpansion
+
+:main
+cls
+title WARP Endpoint IP - Youtube:Misahiro
+echo ############################################################
+echo #                WARP Endpoint IP - Youtube:Misahiro       #
+echo # Like and Subscribe                                       #
+echo # GitHub  : https://github.com/milad7091                   #
+echo # YouTube : https://www.youtube.com/@Misahiro              #
+echo ############################################################
+echo.
+echo 1. WARP IPv4 Endpoint IP 
+echo 2. WARP IPv6 Endpoint IP 
+echo.
+choice /c 120 /M "Select[1,2,3]:"
+if "%errorlevel%"=="3" exit
+if "%errorlevel%"=="2" set ver=v6
+if "%errorlevel%"=="1" set ver=v4
+title WARP IP%ver% Endpoint IP Selected
+set filename=ips-%ver%.txt
+goto get%ver%
+
+
+:getv4
+for /f "delims=" %%i in (%filename%) do (
+set !random!_%%i=randomsort
+)
+for /f "tokens=2,3,4 delims=_.=" %%i in ('set ^| findstr =randomsort ^| sort /m 10240') do (
+call :randomcidrv4
+if not defined %%i.%%j.%%k.!cidr! set %%i.%%j.%%k.!cidr!=anycastip&set /a n+=1
+if !n! EQU 100 goto getip
+)
+goto getv4
+
+:randomcidrv4
+set /a cidr=%random%%%256
+goto :eof
+
+:getv6
+for /f "delims=" %%i in (%filename%) do (
+set !random!_%%i=randomsort
+)
+for /f "tokens=2,3,4 delims=_:=" %%i in ('set ^| findstr =randomsort ^| sort /m 10240') do (
+call :randomcidrv6
+if not defined [%%i:%%j:%%k::!cidr!] set [%%i:%%j:%%k::!cidr!]=anycastip&set /a n+=1
+if !n! EQU 100 goto getip
+)
+goto getv6
+
+:randomcidrv6
+set str=0123456789abcdef
+set /a r=%random%%%16
+set cidr=!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!:!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!:!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!:!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+set /a r=%random%%%16
+set cidr=!cidr!!str:~%r%,1!
+goto :eof
+
+:getip
+del ip.txt > nul 2>&1
+for /f "tokens=1 delims==" %%i in ('set ^| findstr =randomsort') do (
+set %%i=
+)
+for /f "tokens=1 delims==" %%i in ('set ^| findstr =anycastip') do (
+echo %%i>>ip.txt
+)
+for /f "tokens=1 delims==" %%i in ('set ^| findstr =anycastip') do (
+set %%i=
+)
+
+warp
+del ip.txt > nul 2>&1
+echo Please press any key to close the window
+pause > nul
+exit
